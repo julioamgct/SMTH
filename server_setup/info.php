b03c6f071ae7b82ad04837977a59e600f43fd1a8
julioamgct <julioamg.ct@gmail.com>
2026-03-25 07:50:09 -0600

    Update info.php

--- server_setup/info.php
+++ server_setup/info.php
@@ -1,4 +1,6 @@
+Anexo 5: Prueba del procesamiento PHP en tu servidor web
+
+nano /var/www/your_domain/info.php
 <?php
-// Anexo 5: Prueba del procesamiento PHP en tu servidor web
 phpinfo();
-?>
+sudo rm /var/www/your_domain/info.php
